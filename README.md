a8d8834676f1d8853750680e1472ae2690f1610c
Ratimon <ratixoxo@gmail.com>
2023-08-03 16:36:50 +0700

    Feat (./): Adding script to brute foce address with badc0de

---
+++ README.md
@@ -0,0 +1,17 @@
+
+```sh
+poetry run python python_eth_zero_seeker/zero_seeker.py
+```
+
+```sh
+poetry run python python_eth_zero_seeker/badcode.py
+```
+we get:
+```
+Iterations: 2190000
+Iterations: 2191000
+Iterations: 2192000
+Iterations: 2193000
+Found vanity address: 0xc83f8badc0de21b5063bb73d15c78c6ea7839793
+The Private key is:
+```
---
+++ python_eth_zero_seeker/badcode.py
@@ -0,0 +1,27 @@
+import os
+import re
+from ethereum.utils import mk_contract_address, encode_hex, privtoaddr
+
+# find the vanity address with "badc0de" in it
+pattern = re.compile(r'badc0de')
+iterations = 0
+
+while True:
+    # generate a random private key
+    private_key = os.urandom(32)
+    addr = privtoaddr(private_key)
+    contract_addr = encode_hex(mk_contract_address(addr, 0))
+
+    if pattern.search(contract_addr):
+        print(f"Found vanity address: 0x{contract_addr}")
+        print("The Private key is:")
+        print(encode_hex(private_key))
+        break
+
+    iterations += 1
+
+    # Print status every 1000 iterations
+    if iterations % 1000 == 0:
+        print(f"Iterations: {iterations}")
+
+
